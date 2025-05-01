981867a582181f78b592dc991108e0d498f32ae9
erickoghene@gmail.com <erickoghene@gmail.com>
2025-05-01 20:09:59 +0100

    update

--- src/main/java/com/enspy/webtree/services/AuthenticationService.java
+++ src/main/java/com/enspy/webtree/services/AuthenticationService.java
@@ -1,8 +1,21 @@
 package com.enspy.webtree.services;
 
 
+import com.enspy.webtree.dto.responses.ApiResponse;
+import com.enspy.webtree.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private UserRepository userRepository;
+
+    public ApiResponse createUser(){
+        return null;
+    }
+
 }
