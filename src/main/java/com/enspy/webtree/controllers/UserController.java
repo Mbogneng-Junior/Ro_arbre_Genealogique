6a4d0afdaf210a85aca8fd23c4999e1a9063edfc
erickoghene@gmail.com <erickoghene@gmail.com>
2025-05-20 12:03:51 +0100

    update

--- src/main/java/com/enspy/webtree/controllers/UserController.java
+++ src/main/java/com/enspy/webtree/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.enspy.webtree.dto.requests.CreateUserDto;
 import com.enspy.webtree.dto.requests.GetUserInfo;
 import com.enspy.webtree.dto.responses.ApiResponse;
 import com.enspy.webtree.services.AuthenticationService;
+import com.enspy.webtree.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private AuthenticationService authenticationService;
+    private UserService userService;
 
     @PostMapping("/getUserInfo")
     public ResponseEntity<ApiResponse> getUserInfo(@RequestBody GetUserInfo userInfoDto) {
         ApiResponse response = this.authenticationService.getUserInfo(userInfoDto.getUsername());
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(Integer.parseInt(response.getValue())));
+    }
+
+    @PostMapping("/userFamilies")
+    public ResponseEntity<ApiResponse> getUserFamily(@RequestBody GetUserInfo userInfoDto) {
+        ApiResponse response = this.userService.getUserFamilies(userInfoDto.getUsername());
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(Integer.parseInt(response.getValue())));
     }
 }
--- src/main/java/com/enspy/webtree/services/UserService.java
+++ src/main/java/com/enspy/webtree/services/UserService.java
@@ -1,12 +1,15 @@
 package com.enspy.webtree.services;
 
+import com.enspy.webtree.dto.responses.ApiResponse;
+import com.enspy.webtree.models.Family;
+import com.enspy.webtree.models.Users;
 import com.enspy.webtree.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import java.util.*;
 
 
 @Service
@@ -20,6 +23,33 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username).get();
+    }
+
+    public ApiResponse getUserFamilies(String username) {
+        ApiResponse response = new ApiResponse();
+        try {
+            Optional<Users> userOpt = userRepository.findByUsername(username);
+            if (userOpt.isEmpty()) {
+                response.setText("invalid username");
+                response.setValue("404");
+                return response;
+            }
+
+            Users user = userOpt.get();
+            List<Family> families = user.getFamilies();
+            Map<String, String> familyMap = new HashMap<>();
+            for (Family family : families) {
+                familyMap.put(family.getId().toString(), family.getFamilyName());
+            }
+            response.setData(familyMap);
+            response.setText("success");
+            response.setValue("200");
+            return response;
+        } catch (Exception e){
+            response.setText("an error occured :" + e.getMessage());
+            response.setValue("500");
+            return response;
+        }
     }
 
 
